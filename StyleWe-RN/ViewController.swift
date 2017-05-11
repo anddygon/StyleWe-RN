ac6dc86ff1b6a336cc83ef9475a741eedac1f9cc
xiaoP <anddygong@gmail.com>
2017-05-11 18:24:24 +0800

    .

---
+++ StyleWe-RN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StyleWe-RN
+//
+//  Created by xiaoP on 2017/5/11.
+//  Copyright © 2017年 Chicv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
